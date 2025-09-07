e45413b06452cc6c213d6f0d508930b8d6f434d8
IanKilwiny <ribeiroian083@gmail.com>
2025-09-07 14:36:07 -0300

    Adicionando as tabelas no banco de dados

---
+++ database/migrations/2025_09_07_170132_create_equipamentos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipamentos', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->timestamps();
+            $table->string('nome')->nullable();
+            $table->string('tipo')->nullable();
+            $table->integer('quantidade')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipamentos');
+    }
+};
